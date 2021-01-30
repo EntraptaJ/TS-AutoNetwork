65573156a66903fd3b6ad444640f73391b039ef2
KristianFJones <me@kristianjones.dev>
2021-01-30 22:47:16 +0000

    fix: TypeCheck error and log messgae if error

--- src/bin/runActions.ts
+++ src/bin/runActions.ts
@@ -3,13 +3,23 @@ import Container from 'typedi';
 import { logger, LogMode } from '../Library/Logger';
 import { IPAMController } from '../Modules/IPAM/IPAMController';
 
+function isError(obj: Error): obj is Error {
+  if ('message' in obj) {
+    return true;
+  }
+
+  return false;
+}
+
 try {
   const ipamController = Container.get(IPAMController);
 
   const config = await ipamController.loadIPAM('IPAM-Test.yaml');
   logger.log(LogMode.INFO, `Config loaded`, config);
 } catch (err) {
-  console.error(err);
+  if (isError(err)) {
+    console.error(err.message);
+  }
 
   process.exit(1);
 }
