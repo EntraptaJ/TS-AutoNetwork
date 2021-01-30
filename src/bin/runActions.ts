f9b7cedfb23f672ffac3103d2e29ceaba567bd1c
KristianFJones <me@kristianjones.dev>
2021-01-30 22:43:59 +0000

    feat: Exit the process so the Action errors

--- src/bin/runActions.ts
+++ src/bin/runActions.ts
@@ -1,19 +1,7 @@
 // src/bin/runActions.ts
 import Container from 'typedi';
-import { getOctokit } from '@actions/github';
-import { getInput } from '@actions/core';
 import { logger, LogMode } from '../Library/Logger';
 import { IPAMController } from '../Modules/IPAM/IPAMController';
-
-const myToken = getInput('repo-token', {
-  required: false,
-});
-
-let octoKit: any;
-
-if (myToken) {
-  octoKit = getOctokit(myToken);
-}
 
 try {
   const ipamController = Container.get(IPAMController);
@@ -21,5 +9,7 @@ try {
   const config = await ipamController.loadIPAM('IPAM-Test.yaml');
   logger.log(LogMode.INFO, `Config loaded`, config);
 } catch (err) {
-  console.log(err.message);
+  console.error(err);
+
+  process.exit(1);
 }
